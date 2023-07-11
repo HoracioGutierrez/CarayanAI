37f7b7a4702f920d12189df226cd3d0a4184ebb0
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-07-11 11:28:57 -0300

    Add stop and regenerate

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -2,8 +2,8 @@
 
 
 import useCarayanAI from "@/app/hooks/useCarayanAI"
-import { ArrowRightIcon, PaperPlaneIcon } from "@radix-ui/react-icons"
-import { useEffect, useRef } from "react"
+import { PaperPlaneIcon, ReloadIcon, StopIcon } from "@radix-ui/react-icons"
+import { useEffect, useRef, useState } from "react"
 
 type Props = {
     id: string,
@@ -13,7 +13,8 @@ type Props = {
 function Chat({ id, initMessages }: Props) {
 
     const messagesRef = useRef<HTMLTextAreaElement | null>(null)
-    const { append, messages, handleInputChange, input, setMessages } = useCarayanAI()
+    const { append, messages, handleInputChange, input, setMessages , stop , isLoading , setInput } = useCarayanAI()
+    const [showRegenerate, setShowRegenerate] = useState(false)
 
     useEffect(() => {
         if (initMessages && initMessages.length > 0) {
@@ -21,12 +22,19 @@ function Chat({ id, initMessages }: Props) {
         }
     }, [])
 
-    const handleClick = async () => {
+    const handleSend = async () => {
         const messages = messagesRef.current?.value || ""
         if (messages) {
             const message = { content: messages, role: "user" as const, id }
+            setShowRegenerate(false)
+            setInput("")
             await append(message)
         }
+    }
+
+    const handleStop = () => {
+        setShowRegenerate(true)
+        stop()
     }
 
     //TODO agregar la imagen de pablo/usuario dependiendo del rol del mensaje (message.role)
@@ -34,23 +42,31 @@ function Chat({ id, initMessages }: Props) {
     //TODO agregar condicionalmente que el boton cancele el stream
     //TODO agregar condicionalmente un componente que muestre un mensaje inicial de bienvenida con 3 opciones de mensajes y que se pueda elegir uno, el mensaje debe no mostrarse en el chat si hay mensajes para mostrar
 
+    console.log(showRegenerate)
+
     return (
         <>
             <h2 className="text-2xl py-4 px-2">Playground</h2>
             <div className="flex flex-col grow gap-4">
-                <div className=" bg-slate-900 grow border-slate-600 border-2 rounded">
-                    <section className="p-6">
+                <div className=" bg-slate-900 grow border-slate-600 border-2 rounded relative flex flex-col">
+                    <section className="p-6 flex flex-col">
                         {messages.map((message, i) => (
                             <div className="p-2" key={i}>
                                 {message.content}
                             </div>
                         ))}
+                        {showRegenerate && (
+                            <div className="absolute bottom-2 self-center">
+                                <p>Regenerar respuesta</p>
+                                <ReloadIcon/>
+                            </div>
+                        )}
                     </section>
                 </div>
                 <div className="flex w-full border-slate-600 hover:border-slate-300 border-2 rounded-md relative items-center">
                     <textarea ref={messagesRef} onChange={handleInputChange} value={input} className="resize-none bg-slate-900 text-wite rounded-l-md w-full"></textarea>
-                    <button className="bg-slate-900 rounded-r-md absolute right-2" onClick={handleClick}>
-                        <PaperPlaneIcon/>
+                    <button className="bg-slate-900 rounded-r-md absolute right-2 z-10" onClick={isLoading ? handleStop : handleSend }>
+                        {isLoading ? <StopIcon/> : <PaperPlaneIcon/>}
                     </button>
                 </div>
             </div>
