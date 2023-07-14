5bc38d0ecfbf47fcef3d23858f47423a861b64a3
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-07-14 18:13:27 -0300

    fix

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -8,10 +8,7 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
             authorization: {
                 params: {
-                    prompt: "consent",
-                    access_type: "offline",
-                    response_type: "code",
-                    redirect_uri: "https://carayanai-manual.vercel.app/api/auth/callback/google"
+                    redirect_uri: "https://carayanai-manual.vercel.app/api/auth/callback/google",
                 },
                 url: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code&redirect_uri=https%3A%2F%2Fcarayanai-manual.vercel.app%2Fapi%2Fauth%2Fcallback%2Fgoogle"
             }
