7fbd33be3abca90f1201810614c237bc68a5c2ec
Horacio Gutierrez <horacio.estevez@gmail.com>
2023-07-14 17:23:46 -0300

    Add authorization url

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -11,7 +11,8 @@ export const authOptions: NextAuthOptions = {
                     prompt: "consent",
                     access_type: "offline",
                     response_type: "code"
-                }
+                },
+                url: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code&redirect_uri=http%3A%2F%2Fcarayanai-manual.vercel.app%3A3000%2Fapi%2Fauth%2Fcallback%2Fgoogle"
             }
         }),        
     ],
